3325abbbc80dcb7bb9f89ba4bfa5c3856bba6d81
baydullod <baydullod@gmail.com>
2021-05-20 01:33:02 +0000

    added module

---
+++ module/db_module.tf
@@ -0,0 +1,34 @@
+module "wordpress_db" {
+source = "../"
+region              = "us-east-2"
+identifier          = "dbname"
+allocated_storage   = 20
+storage_type        = "gp2"
+engine              = "mysql"
+engine_version      = "5.7"
+instance_class      = "db.t2.micro"
+name                = "mydb"
+username            = "foo"
+publicly_accessible = true
+subnet_ids = [
+			"subnet-0d2764c89c160a350", 
+			"subnet-0b2fcce258646b51f", 
+			"subnet-06d0f3ddb7570f914"
+]
+allowed_hosts = [
+    "50.194.68.230/32",
+    "127.0.0.1/32"
+]
+}
+output "region" {
+	value = module.wordpress_db.region
+}
+output "subnet_list" {
+	value = module.wordpress_db.subnet_list
+}
+output "allowed_hosts" {
+	value = module.wordpress_db.allowed_hosts
+}
+output "wordpress_db_NAME" {
+	value = module.wordpress_db.DB_NAME
+}
